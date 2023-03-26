2c9d6ff7911c39596b84e89241bee1d119982ab8
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-03-26 15:50:46 -0400

    conversation.js
    Add ParseInt()

---
+++ conversion.js
@@ -0,0 +1,8 @@
+// In JavaScript, parseInt, parseFloat, and toFixed are all methods that can be used to manipulate numbers.
+
+/** parseInt is a function that converts a string to an integer.It takes two arguments: 
+  *the string to convert and the radix, which is the base number system to use(typically either 10 for decimal or 16 for hexadecimal).
+  *If the radix is not specified, parseInt assumes base 10.
+**/
+
+// parseFloat is similar to parseInt, but it converts a string to a floating - point number.It only takes one argument, the string to convert.
