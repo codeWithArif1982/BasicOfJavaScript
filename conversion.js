9e834e3bfd6a520cdd0ad26e03b8718e3f30fc48
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-03-26 15:57:29 -0400

    Update conversion.js
    Add toFixed method

--- conversion.js
+++ conversion.js
@@ -7,4 +7,14 @@
 
 // parseFloat is similar to parseInt, but it converts a string to a floating - point number.It only takes one argument, the string to convert.
 const floatString = "3.14";
-const floatNum = parseFloat(floatString); // 3.14
+const floatNum = parseFloat(floatString); 
+console.log(floatNum); // 3.14
+
+/** toFixed is a method that rounds a number to a specified number of decimal places and returns a string representation of the result.
+  *It takes one argument, the number of decimal places to round to.
+**/
+
+const num = 3.14159265359;
+const roundedNum = num.toFixed(2);
+console.log(roundNum) // "3.14"
+console.log(num.toFixed(4)); // "3.1415"
