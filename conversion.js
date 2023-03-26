fb11f6881845689dd6e5f4ed31a4a8b2042289bb
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-03-26 15:51:55 -0400

    Update conversion.js
    Add parseFloat

--- conversion.js
+++ conversion.js
@@ -6,3 +6,5 @@
 **/
 
 // parseFloat is similar to parseInt, but it converts a string to a floating - point number.It only takes one argument, the string to convert.
+const floatString = "3.14";
+const floatNum = parseFloat(floatString); // 3.14
