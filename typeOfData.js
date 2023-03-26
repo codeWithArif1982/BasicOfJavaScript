98f74b7aad2c75263f3148f16e95284e1dbe0dea
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-03-26 15:41:04 -0400

    Create typeOfData.js
    add all the data type

---
+++ typeOfData.js
@@ -0,0 +1,17 @@
+// What will be the output of the following code?
+var x;
+var y = true;
+var z = 10;
+var str = "Hello, World!";
+var obj = { name: "John", age: 30 };
+var nullValue = null;
+function add(a, b) {
+    return a + b;
+}
+console.log(typeof x); // A. "undefined"
+console.log(typeof y); // B. "boolean"
+console.log(typeof z); // C. "number"
+console.log(typeof str); // D. "string"
+console.log(typeof obj); // E. "object"
+console.log(typeof nullValue); // F. "object"
+console.log(typeof add); // G. "function"
